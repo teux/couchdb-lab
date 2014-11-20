dcdc27a461625ad7f235b4fe2dcb04223de47322
teux <github@teux.ru>
2014-11-20 19:03:16 +0300

    Filter on _deleted

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ require('./components');
 angular.module('App', ['angular.bundle', 'app.components'])
     .controller('AppController', function ($scope, $compile, pouchdb) {
         var localdb = 'testdb1';
-        var remotedb = 'http://teux.iriscouch.com/' + localdb;
+        var remotedb = 'http://192.168.0.9:5984/' + localdb;
         var docs = $scope.docs = pouchdb.create(localdb);
 
         var setItemCount = function () {
@@ -11,36 +11,28 @@ angular.module('App', ['angular.bundle', 'app.components'])
                 $scope.itemCount = info.doc_count;
             });
         };
-        var onChange = function () {
+        var pushChange = function () {
             docs.replicate.to(remotedb);
             setItemCount();
         };
-
-        setItemCount();
-
-        docs.replicate.from(remotedb, {live: true})
+        var livePull = function () {
+            docs.replicate.from(remotedb, {
+                live: true,
+                filter: 'coauthor/latest'
+            })
             .on('change', function () {
                 setItemCount();
             })
-            .on('complete', function () {
-                docs.replicate.to(remotedb);
-            })
-            .on('error', function (err) {
-                console.log(err);
-            });
-
+        };
         $scope.addDoc = function () {
-            docs.post({timestamp: Date.now()}).then(onChange);
+            docs.post({timestamp: Date.now()}).then(pushChange);
         };
-
         $scope.modifyDoc = function (doc) {
-            docs.put(doc).then(onChange);
+            docs.put(doc).then(pushChange);
         };
-
         $scope.removeDoc = function (item) {
-            docs.remove(item).then(onChange);
+            docs.remove(item).then(pushChange);
         };
-
         $scope.removeDocs = function () {
             docs.allDocs({include_docs: true})
                 .then(function (response) {
@@ -52,13 +44,24 @@ angular.module('App', ['angular.bundle', 'app.components'])
                         };
                     }));
                 })
-                .then(onChange);
+                .then(pushChange);
         };
-
         $scope.getPhotoUrl = function (doc) {
             return docs.getAttachment(doc._id, 'photo')
                 .then(function (blob) {
                     return blobUtil.createObjectURL(blob);
                 });
-        }
+        };
+
+        // Initial replication from
+        docs.replicate.from(remotedb, {
+            filter: 'coauthor/latest'
+        })
+        .on('complete', function () {
+            setItemCount();
+            livePull();
+            docs.replicate.to(remotedb);
+        });
+
+        setItemCount();
     });
